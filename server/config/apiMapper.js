0cd49d8b912856626aa1d4d4987049e16156cc46
Proseed Banerjee <banerjee.proseed@senrysa.com>
2023-01-24 16:59:04 +0530

    [edit] resolved login password issue

--- server/config/apiMapper.js
+++ server/config/apiMapper.js
@@ -2,6 +2,8 @@ function apiMapper(requestBody) {
   switch (requestBody.name) {
     case 'R_GET_ONDC_ORDERS':
       return { method: 'post', params: requestBody.params, url: 'order-ms/ondc_dashboard/orders' };
+      case 'R_GET_ONDC_ORDERS_CSV':
+        return { method: 'post', params: requestBody.params, url: 'order-ms/ondc_dashboard/orders' };
     default:
       return null;
   }

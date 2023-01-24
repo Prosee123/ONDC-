46b4efca64fe6ca14d05189b7ef82475ac3c41aa
nn-karthik <karthik.n@mrcooper.com>
2023-01-24 22:03:08 +0530

    Adding client token for staging requests

--- server/services/intercept.service.js
+++ server/services/intercept.service.js
@@ -1,27 +1,30 @@
 const { default: axios } = require('axios');
 const apiMapper = require('../config/apiMapper');
-const { gateway } = require('../config/config');
+const { gateway,gatewayClientToken } = require('../config/config');
 
 const processInterceptRequest = async (requestPayload) => {
-    const mappedObj = apiMapper(requestPayload);
+  const mappedObj = apiMapper(requestPayload);
   if (mappedObj) {
     const options = {
-      url: `${gateway}/api/${mappedObj.url}`,
+      url: `${gateway}/${mappedObj.url}`,
       method: mappedObj.method,
       data: mappedObj.params,
       headers: {
         Accept: '*/*',
+        // 'Content-Disposition': "attachment",
         'Cache-Control': 'no-cache',
         'Access-Control-Allow-Origin': '*',
+        ...(gatewayClientToken && { 'Client-Token': gatewayClientToken }),
       },
     };
+    console.log(options);
     return axios(options)
-        .then((res) => {
+      .then((res) => {
         return { status: res.status, body: res.data };
       })
       .catch((err) => {
-          console.error("intercept.service.error:",err)
-          const errorObj = {};
+        console.error('intercept.service.error:', err);
+        const errorObj = {};
         if (err.data) {
           errorObj.status = err.data.status;
           errorObj.body = err.data;
